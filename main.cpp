97259d44869518b9a55377a63d78eb1ae4a6fefb
Alexander Makarov <makarasan@mail.ru>
2020-06-03 13:28:56 +0800

    Проверка QtCreator

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main() {
     image.set(52, 41, red);
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+    //
     return 0;
 }
